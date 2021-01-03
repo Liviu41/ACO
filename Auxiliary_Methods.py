600c305f3728005295e2ed583b5c640849de00f4
Liviu Rujan <liviu_rujan@yahoo.com>
2021-01-03 18:36:00 +0200

    Preprocessing
    Still no ACO implementation. Refined code and added helper methods in auxiliary,

--- Auxiliary_Methods.py
+++ Auxiliary_Methods.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def read_HSI(verbose = False):
     X = loadmat('C:\Projects\ACO\DB\Indian_Pines\Indian_pines_corrected.mat')['indian_pines_corrected']
@@ -21,3 +22,18 @@ def extract_pixels(X, y, verbose = False):
     if verbose == True:
         df.head()
     return df
+
+
+# method to display an image based on a band of an HSI
+def HSI_band_show(X, band):
+    # show image
+    scaledData = np.double(X)
+    scaledData = (scaledData - np.min(scaledData)) / (np.max(scaledData) 
+                                                      - np.min(scaledData)) 
+    scaledDataForVisualising = scaledData.transpose(2, 0, 1)
+    f, axarr = plt.subplots(1,1) 
+    axarr.imshow(scaledDataForVisualising[band], cmap='jet')
+    # uncomment to show multiple bands
+    # axarr[1].imshow(scaledDataForVisualising[198], cmap='jet')
+    plt.show()
+    # show image
--- Core.py
+++ Core.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
@@ -6,56 +5,45 @@ from sklearn.neighbors import KNeighborsClassifier
 import scikitplot as skplt
 import plotly.graph_objects as go
 from tqdm import tqdm
-from Auxiliary_Methods import *
+import Auxiliary_Methods as am
+import matplotlib.pyplot as plt
 
+X, y = am.read_HSI()
 
-X, y = read_HSI()
-
-# show image
-scaledData = np.double(X)
-scaledData = (scaledData - np.min(scaledData)) / (np.max(scaledData) 
-                                                  - np.min(scaledData)) 
-scaledDataForVisualising = scaledData.transpose(2, 0, 1)
-f, axarr = plt.subplots(1,4) 
-axarr[0].imshow(scaledDataForVisualising[199], cmap='jet')
-axarr[1].imshow(scaledDataForVisualising[198], cmap='jet')
-axarr[2].imshow(scaledDataForVisualising[197], cmap='jet')
-axarr[3].imshow(scaledDataForVisualising[196], cmap='jet')
-plt.show()
-# show image
+am.HSI_band_show(X, 180)
 
 # The below method extracts the pixels from the HSI and saves into CSV 
-df = extract_pixels(X, y)
+df = am.extract_pixels(X, y)
 
 # X = (145*145, 200), y = (145*145, 1)
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
-X.shape, y.shape
+X_flat = df.iloc[:, :-1].values
+y_flat = df.iloc[:, -1].values
+X_flat.shape, y_flat.shape
 
-X_train, X_test, y_train, y_test, indices_train, indices_test  = train_test_split(X, y,  range(X.shape[0]), 
+X_train_flat, X_test_flat, y_train_flat, y_test_flat, indices_train_flat, indices_test_flat  = train_test_split(X_flat, y_flat,  range(X_flat.shape[0]), 
                                                                                   test_size = 0.8, random_state = 12)
-X_train.shape, X_test.shape
+X_train_flat.shape, y_train_flat.shape
 
 
 knn = KNeighborsClassifier(n_neighbors=16)
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
+knn.fit(X_train_flat, y_train_flat)
+y_pred_flat = knn.predict(X_test_flat)
 
-print(f'Accuracy: {accuracy_score(y_test, y_pred)}%')
+print(f'Accuracy: {accuracy_score(y_test_flat, y_pred_flat)}%')
 
 skplt.metrics.plot_confusion_matrix(
-    y_test, 
-    y_pred,
+    y_test_flat, 
+    y_pred_flat,
     figsize=(12,12));
 
 fig = go.Figure(data=go.Heatmap(
-                   z= confusion_matrix(y_test, y_pred),
+                   z= confusion_matrix(y_test_flat, y_pred_flat),
                    x=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
                    y=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
                    hoverongaps = False))
 fig.show()
 
-print('Classification report:\n',classification_report(y_test,y_pred))
+print('Classification report:\n',classification_report(y_test_flat,y_pred_flat))
 
 plt.figure(figsize=(8, 6))
 plt.imshow(df.iloc[:, -1].values.reshape((145, 145)), cmap='jet')
@@ -65,21 +53,21 @@ plt.title('Ground Truth')
 plt.savefig('ground_truth.png')
 plt.show()
 
-pre = y_pred
+pre = y_pred_flat
 
-clmap = [0]*X.shape[0]
+clmap = [0]*X_flat.shape[0]
 
-for i in tqdm(range(len(indices_train))):
-    clmap[indices_train[i]] = y[indices_train[i]]
+for i in tqdm(range(len(indices_train_flat))):
+    clmap[indices_train_flat[i]] = y_flat[indices_train_flat[i]]
 
-for i in tqdm(range(len(indices_test))):
-    clmap[indices_test[i]] = pre[i]
+for i in tqdm(range(len(indices_test_flat))):
+    clmap[indices_test_flat[i]] = pre[i]
 
 plt.figure(figsize=(8, 6))
 plt.imshow(np.array(clmap).reshape((145, 145)), cmap='jet')
 plt.colorbar()
 plt.axis('off')
-plt.title('Classification Map (PCA + KNeighborsClassifier)')
+plt.title('Classification Map (KNeighborsClassifier)')
 plt.savefig('Classification_map.png')
 plt.show()
 
