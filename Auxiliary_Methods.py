aa7a8613bc3dd0d4fabaa41ba4d959f5a0749263
Liviu Rujan <liviu_rujan@yahoo.com>
2021-01-04 20:35:51 +0200

    Theta and encapsulations
    Defined pheromone and improved readability.

--- Auxiliary_Methods.py
+++ Auxiliary_Methods.py
@@ -2,6 +2,9 @@ from scipy.io import loadmat
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import scikitplot as skplt
+import plotly.graph_objects as go
 
 def read_HSI(verbose = False):
     X = loadmat('C:\Projects\ACO\DB\Indian_Pines\Indian_pines_corrected.mat')['indian_pines_corrected']
@@ -37,3 +40,20 @@ def HSI_band_show(X, band):
     # axarr[1].imshow(scaledDataForVisualising[198], cmap='jet')
     plt.show()
     # show image
+    
+def scores(df, y_test_flat, y_pred_flat):
+    print(f'Accuracy: {accuracy_score(y_test_flat, y_pred_flat)}%')
+    
+    skplt.metrics.plot_confusion_matrix(
+        y_test_flat, 
+        y_pred_flat,
+        figsize=(12,12));
+    
+    fig = go.Figure(data=go.Heatmap(
+                       z= confusion_matrix(y_test_flat, y_pred_flat),
+                       x=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
+                       y=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
+                       hoverongaps = False))
+    fig.show()
+    
+    print('Classification report:\n',classification_report(y_test_flat,y_pred_flat))
--- Core.py
+++ Core.py
@@ -1,9 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-import scikitplot as skplt
-import plotly.graph_objects as go
 from tqdm import tqdm
 import Auxiliary_Methods as am
 import matplotlib.pyplot as plt
@@ -20,33 +17,19 @@ X_flat = df.iloc[:, :-1].values
 y_flat = df.iloc[:, -1].values
 X_flat.shape, y_flat.shape
 
-X_train_flat, X_test_flat, y_train_flat, y_test_flat, indices_train_flat, indices_test_flat  = train_test_split(X_flat, y_flat,  range(X_flat.shape[0]), 
-                                                                                  test_size = 0.8, random_state = 12)
+X_train_flat, X_test_flat, y_train_flat, y_test_flat, indices_train_flat, indices_test_flat  = train_test_split(X_flat, y_flat,
+                                                            range(X_flat.shape[0]), test_size = 0.2, random_state = 12, stratify=y_flat)
 X_train_flat.shape, y_train_flat.shape
 
 
-knn = KNeighborsClassifier(n_neighbors=16)
+knn = KNeighborsClassifier(n_neighbors=8)
 knn.fit(X_train_flat, y_train_flat)
 y_pred_flat = knn.predict(X_test_flat)
 
-print(f'Accuracy: {accuracy_score(y_test_flat, y_pred_flat)}%')
-
-skplt.metrics.plot_confusion_matrix(
-    y_test_flat, 
-    y_pred_flat,
-    figsize=(12,12));
-
-fig = go.Figure(data=go.Heatmap(
-                   z= confusion_matrix(y_test_flat, y_pred_flat),
-                   x=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
-                   y=[f'class-{i}' for i in np.unique(df.loc[:, 'class'].values)],
-                   hoverongaps = False))
-fig.show()
-
-print('Classification report:\n',classification_report(y_test_flat,y_pred_flat))
+am.scores(df, y_test_flat, y_pred_flat)
 
 plt.figure(figsize=(8, 6))
-plt.imshow(df.iloc[:, -1].values.reshape((145, 145)), cmap='jet')
+plt.imshow(df.iloc[:, -1].values.reshape((X.shape[0], X.shape[0])), cmap='jet')
 plt.colorbar()
 plt.axis('off')
 plt.title('Ground Truth')
@@ -64,12 +47,17 @@ for i in tqdm(range(len(indices_test_flat))):
     clmap[indices_test_flat[i]] = pre[i]
 
 plt.figure(figsize=(8, 6))
-plt.imshow(np.array(clmap).reshape((145, 145)), cmap='jet')
+plt.imshow(np.array(clmap).reshape((X.shape[0], X.shape[0])), cmap='jet')
 plt.colorbar()
 plt.axis('off')
 plt.title('Classification Map (KNeighborsClassifier)')
 plt.savefig('Classification_map.png')
 plt.show()
+
+no_of_classes = np.unique(y)
+theta = np.zeros((X.shape[0], X.shape[0], no_of_classes.size))
+
+pred_classes = np.unique(pre)
 
 
 
